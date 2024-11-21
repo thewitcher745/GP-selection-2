289f5a9a1e01617fff14793553b2dabd7144402b
thewitcher745 <thewitcher745@gmail.com>
2024-11-21 18:35:33 +0330

    FIX: Bugfix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 import reports.base_report as base_report
-import reports.final_report as final_report
 
 base_report_df: pd.DataFrame = base_report.base_report_df
-final_report_df: pd.DataFrame = final_report.final_report_df
-
 base_report_df.to_excel("BaseReport.xlsx")
+
+import reports.final_report as final_report
+
+final_report_df: pd.DataFrame = final_report.final_report_df
 final_report_df.to_excel("FinalReport.xlsx")
