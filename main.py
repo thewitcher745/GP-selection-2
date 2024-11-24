03a41cf1e60c58fb08f7dec9f83529185872bd63
thewitcher745 <thewitcher745@gmail.com>
2024-11-25 00:42:15 +0330

    CHORE: Added printed output log

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import reports.base_report as base_report
 base_report_df: pd.DataFrame = base_report.base_report_df
 base_report_df.to_excel("BaseReport.xlsx")
 
+print("BaseReport.xlsx created.")
+
 import reports.final_report as final_report
 
 final_report_df: pd.DataFrame = final_report.final_report_df
 final_report_df.to_excel("FinalReport.xlsx")
+
+print("FinalReport.xlsx created.")
