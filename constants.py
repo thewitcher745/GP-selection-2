fc4f69d44eb7ed9fd294a5a1198fd1555bd4d5f9
thewitcher745 <thewitcher745@gmail.com>
2025-01-14 22:24:08 +0330

    FEAT: Added runtime argument for pair_list file

--- constants.py
+++ constants.py
@@ -1,8 +1,19 @@
+import argparse
 from dotenv import dotenv_values
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--pl', type=str, help='File name of the positions to process')
+
+# Parse arguments
+args = parser.parse_args()
 
 params = dotenv_values(".env.params")
 
-excluded_pairs = ["REEFUSDT"]
+excluded_pairs = [""]
 
 max_final_report_pairs = 30
 capital_per_trade = 100
+
+# Set the positions file name from the argument
+positions_file_name = f'./{args.pl}' if args.pl else "./all_positions.xlsx"
